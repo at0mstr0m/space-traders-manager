a99e815b9517f6ed19c061c3c28213a56d43e08c
at0mstr0m <jan.u.hellwig@gmail.com>
2023-09-22 23:32:15 +0200

    trait EnumToArray

---
+++ app/Traits/EnumToArray.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Traits;
+
+// https://stackoverflow.com/a/71680007/13128152
+trait EnumToArray
+{
+
+    public static function names(): array
+    {
+        return array_column(self::cases(), 'name');
+    }
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+
+    public static function toArray(): array
+    {
+        return array_combine(self::names(), self::values());
+    }
+}
