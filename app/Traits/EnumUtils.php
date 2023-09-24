5bf6562c50faba7fb2ffdcf4deab407bb81deaba
at0mstr0m <jan.u.hellwig@gmail.com>
2023-09-24 18:23:58 +0200

    add comment

--- app/Traits/EnumUtils.php
+++ app/Traits/EnumUtils.php
@@ -4,20 +4,21 @@ declare(strict_types=1);
 
 namespace App\Traits;
 
-// https://stackoverflow.com/a/71680007/13128152
 trait EnumUtils
 {
-
+    // https://stackoverflow.com/a/71680007/13128152
     public static function names(): array
     {
         return array_column(self::cases(), 'name');
     }
 
+    // https://stackoverflow.com/a/71680007/13128152
     public static function values(): array
     {
         return array_column(self::cases(), 'value');
     }
 
+    // https://stackoverflow.com/a/71680007/13128152
     public static function toArray(): array
     {
         return array_combine(self::names(), self::values());
