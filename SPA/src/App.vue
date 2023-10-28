2d3cc6ba6e86782aca6e4ecc31c0ddbe0111ef39
at0mstr0m <jan.u.hellwig@gmail.com>
2023-10-28 11:19:35 +0200

    created vuetify SPA

---
+++ SPA/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <router-view />
+</template>
+
+<script setup>
+  import axios from 'axios';
+  axios.get('http://localhost/api/').then((res) => {
+    console.log(res);
+  });
+  console.log('Bingo Bongo');
+</script>
---
+++ run-spa-dev.sh
@@ -0,0 +1,1 @@
+docker run -p 3000:3000 -it --rm -v $(pwd)/SPA:/app -w /app oven/bun run dev --host
