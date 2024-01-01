41efe89df4553a10b3c7d0ccaa9b85dcce6f1a79
at0mstr0m <jan.u.hellwig@gmail.com>
2024-01-01 16:21:52 +0100

    refactoring

--- app/Jobs/ServeTradeRoute.php
+++ app/Jobs/ServeTradeRoute.php
@@ -1,56 +1,35 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Jobs;
 
 use App\Actions\UpdateOrRemovePotentialTradeRoutesAction;
 use App\Enums\TradeSymbols;
-use App\Helpers\SpaceTraders;
 use App\Models\PotentialTradeRoute;
 use App\Models\Ship;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Foundation\Bus\PendingDispatch;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
 
-class ServeTradeRoute implements ShouldQueue
+class ServeTradeRoute extends ShipJob
 {
-    use Dispatchable;
-    use InteractsWithQueue;
-    use Queueable;
-    use SerializesModels;
-
-    private SpaceTraders $api;
-
     /**
      * Create a new job instance.
      */
     public function __construct(
-        private string $shipSymbol,
+        protected string $shipSymbol,
         private string $origin,
         private string $destination,
         private TradeSymbols $tradedGood,
-        private ?Ship $ship = null,
+        protected ?Ship $ship = null,
     ) {
-        $this->api = app(SpaceTraders::class);
+        $this->constructorParams = func_get_args();
     }
 
     /**
      * Execute the job.
      */
-    public function handle(): void
+    public function handleShip(): void
     {
-        $this->initShip();
-
         dump("{$this->ship->symbol} serving trade route {$this->origin} -> {$this->destination} with {$this->tradedGood->value}");
-
-        if ($this->ship->is_in_transit || $this->ship->cooldown) {
-            dump("{$this->ship->symbol} is in transit or on cooldown");
-            $this->selfDispatch()->delay($this->ship->cooldown);
-
-            return;
-        }
 
         if ($this->ship->cargo_is_empty) {
             dump("{$this->ship->symbol} cargo is empty");
@@ -90,52 +69,21 @@ class ServeTradeRoute implements ShouldQueue
             $this->flyToLocation($this->origin);
 
             return;
-        } else {
-            dump("{$this->ship->symbol} cargo is not empty");
-            if ($this->ship->waypoint_symbol === $this->destination) {
-                dump("{$this->ship->symbol} sell cargo {$this->tradedGood->value}");
-                $this->ship->sellCargo($this->tradedGood);
-                dump("{$this->ship->symbol} fly to {$this->origin}");
-                $this->flyToLocation($this->origin);
-
-                return;
-            }
-            dump("{$this->ship->symbol} fly to {$this->destination}");
-            $this->flyToLocation($this->destination);
+        }
+        dump("{$this->ship->symbol} cargo is not empty");
+        if ($this->ship->waypoint_symbol === $this->destination) {
+            dump("{$this->ship->symbol} sell cargo {$this->tradedGood->value}");
+            $this->ship->sellCargo($this->tradedGood);
+            dump("{$this->ship->symbol} fly to {$this->origin}");
+            $this->flyToLocation($this->origin);
 
             return;
         }
+        dump("{$this->ship->symbol} fly to {$this->destination}");
+        $this->flyToLocation($this->destination);
+
+        return;
 
         dump('did not match any conditions');
-    }
-
-    private function initShip(): void
-    {
-        $this->ship ??= Ship::findBySymbol($this->shipSymbol);
-        if (!$this->ship) {
-            throw new \Exception("Ship not found: {$this->shipSymbol}");
-        }
-        $this->ship = $this->ship->refetch();
-    }
-
-    private function flyToLocation(string $waypointSymbol): void
-    {
-        dump("fly to {$waypointSymbol}");
-        $cooldown = $this->ship
-            ->refuel()
-            ->navigateTo($waypointSymbol)
-            ->cooldown;
-        $this->selfDispatch()->delay($cooldown);
-    }
-
-    private function selfDispatch(): PendingDispatch
-    {
-        return static::dispatch(
-            $this->shipSymbol,
-            $this->origin,
-            $this->destination,
-            $this->tradedGood,
-            $this->ship,
-        );
     }
 }
---
+++ app/Jobs/ShipJob.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Jobs;
+
+use App\Helpers\SpaceTraders;
+use App\Models\Ship;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Foundation\Bus\PendingDispatch;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+abstract class ShipJob implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    protected array $constructorParams = [];
+
+    private ?SpaceTraders $api = null;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(
+        protected string $shipSymbol,
+        protected ?Ship $ship = null,
+    ) {
+        $this->constructorParams = func_get_args();
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $this->initShip();
+
+        if ($this->ship->is_in_transit || $this->ship->cooldown) {
+            dump("{$this->ship->symbol} is in transit or on cooldown");
+            $this->selfDispatch()->delay($this->ship->cooldown);
+
+            return;
+        }
+
+        $this->handleShip();
+    }
+
+    abstract protected function handleShip(): void;
+
+    protected function selfDispatch(array $params = []): PendingDispatch
+    {
+        return static::dispatch(
+            ...$this->constructorParams,
+            ...$params
+        );
+    }
+
+    protected function flyToLocation(string $waypointSymbol): void
+    {
+        dump("fly to {$waypointSymbol}");
+        $cooldown = $this->ship
+            ->refuel()
+            ->navigateTo($waypointSymbol)
+            ->cooldown;
+        $this->selfDispatch()->delay($cooldown);
+    }
+
+    protected function initApi(): void
+    {
+        $this->api ??= app(SpaceTraders::class);
+    }
+
+    private function initShip(): void
+    {
+        $this->ship ??= Ship::findBySymbol($this->shipSymbol);
+        if (!$this->ship) {
+            throw new \Exception("Ship not found: {$this->shipSymbol}");
+        }
+        $this->ship = $this->ship->refetch();
+    }
+}
