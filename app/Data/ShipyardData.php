17c89c5e5900dab9cf7364ad8035c5dd4a126dd3
at0mstr0m <jan.u.hellwig@gmail.com>
2024-02-14 13:51:38 +0100

    fix optional parameter $shipTypes declared before required parameter

--- app/Data/ShipyardData.php
+++ app/Data/ShipyardData.php
@@ -14,13 +14,13 @@ class ShipyardData extends Data implements GeneratableFromResponse
 {
     public function __construct(
         public string $symbol,
+        public int $modificationsFee,
         #[DataCollectionOf(ShipTypeData::class)]
         public ?DataCollection $shipTypes = null,
         #[DataCollectionOf(TransactionData::class)]
         public ?DataCollection $transactions = null,
         #[DataCollectionOf(ShipyardShipData::class)]
         public ?DataCollection $ships = null,
-        public int $modificationsFee,
     ) {}
 
     public static function fromResponse(array $response): static
@@ -31,10 +31,10 @@ class ShipyardData extends Data implements GeneratableFromResponse
 
         return new static(
             symbol: $response['symbol'],
+            modificationsFee: $response['modificationsFee'],
             shipTypes: ShipTypeData::collectionFromResponse($response['shipTypes']),
             transactions: TransactionData::collectionFromResponse(data_get($response, 'transactions', [])),
             ships: ShipyardShipData::collectionFromResponse($ships),
-            modificationsFee: $response['modificationsFee'],
         );
     }
 }
