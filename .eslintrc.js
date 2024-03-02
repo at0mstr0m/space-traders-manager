7e11b8d272bad38fc86d974e1899cc042651bff6
at0mstr0m <jan.u.hellwig@gmail.com>
2024-03-02 09:27:43 +0100

    Update ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,14 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
+    "plugin:vue/vue3-essential",
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended",
   ],
-}
+  rules: {
+    "no-console": 0,
+    "no-empty": 0,
+    "no-irregular-whitespace": 0,
+    "arrow-parens": ["error", "always"],
+  },
+};
