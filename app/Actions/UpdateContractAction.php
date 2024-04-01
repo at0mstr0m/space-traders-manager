bdb09c82fea532995c53811286026babcc1b8542
at0mstr0m <jan.u.hellwig@gmail.com>
2024-04-01 22:33:49 +0200

    Add strict types declaration and update contract and survey actions

--- app/Actions/UpdateContractAction.php
+++ app/Actions/UpdateContractAction.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Actions;
 
-use App\Models\Agent;
 use App\Data\ContractData;
 use App\Data\DeliveryData;
+use App\Models\Agent;
 use App\Models\Contract;
 use Lorisleiva\Actions\Concerns\AsAction;
 
@@ -18,7 +20,6 @@ class UpdateContractAction
         $contract = $agent->contracts()->updateOrCreate(
             ['identification' => $contractData->identification],
             [
-                'identification' => $contractData->identification,
                 'faction_symbol' => $contractData->factionSymbol,
                 'type' => $contractData->type,
                 'accepted' => $contractData->accepted,
@@ -33,17 +34,13 @@ class UpdateContractAction
         // update its deliveries
         $contractData->deliveries
             ->each(
-                fn (DeliveryData $deliveryData) =>
-                $contract->deliveries()->updateOrCreate(
+                fn (DeliveryData $deliveryData) => $contract->deliveries()->updateOrCreate(
                     [
                         'trade_symbol' => $deliveryData->tradeSymbol,
                         'destination_symbol' => $deliveryData->destinationSymbol,
                         'units_required' => $deliveryData->unitsRequired,
                     ],
                     [
-                        'trade_symbol' => $deliveryData->tradeSymbol,
-                        'destination_symbol' => $deliveryData->destinationSymbol,
-                        'units_required' => $deliveryData->unitsRequired,
                         'units_fulfilled' => $deliveryData->unitsFulfilled,
                     ]
                 )
