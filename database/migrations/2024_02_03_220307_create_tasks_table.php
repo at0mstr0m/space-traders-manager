e569f9dea22254e75cabe89a4cd6d6c6b8c855b7
at0mstr0m <jan.u.hellwig@gmail.com>
2024-06-13 19:10:19 +0200

    Refactor tasks table migration to remove default value for payload column

--- database/migrations/2024_02_03_220307_create_tasks_table.php
+++ database/migrations/2024_02_03_220307_create_tasks_table.php
@@ -15,8 +15,7 @@ return new class() extends Migration {
             $table->id();
             $table->timestamps();
             $table->enum('type', TaskTypes::values());
-            $table->json('payload')
-                ->default('{}');
+            $table->json('payload');
         });
 
         Schema::table('ships', function (Blueprint $table) {
