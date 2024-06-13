e6f7579b8bb230b25d51e44bc6a875f843331f9c
at0mstr0m <jan.u.hellwig@gmail.com>
2024-06-13 18:57:00 +0200

    Add FirebaseDownload command to download data from Firebase

---
+++ app/Console/Commands/FirebaseDownload.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use App\Actions\Firebase\DownloadRelationsPotentialTradeRoutesToShipsAction;
+use App\Actions\Firebase\DownloadTasksAction;
+use App\Actions\Firebase\DownloadTaskShipRelationsAction;
+use Illuminate\Console\Command;
+
+class FirebaseDownload extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'firebase-download {--purge}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Download data from Firebase.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        /** @var bool */
+        $purge = $this->option('purge');
+
+        $this->components->info('Downloading Tasks from Firebase...');
+        DownloadTasksAction::run($purge);
+        $this->components->info('Tasks downloaded from Firebase.');
+
+        $this->components->info('Downloading TaskShipRelations from Firebase...');
+        DownloadTaskShipRelationsAction::run($purge);
+        $this->components->info('TaskShipRelations downloaded from Firebase.');
+
+        $this->components->info('Downloading Relations of PotentialTradeRoutes to Ships from Firebase...');
+        DownloadRelationsPotentialTradeRoutesToShipsAction::run($purge);
+        $this->components->info('Relations of PotentialTradeRoutes to Ships downloaded from Firebase.');
+
+        $this->components->info('Done downloading data from Firebase.');
+    }
+}
