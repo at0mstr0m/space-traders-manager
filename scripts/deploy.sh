0c6f4e7bdd741075275ddc7a3039f63d2c321675
at0mstr0m <jan.u.hellwig@gmail.com>
2024-06-16 14:30:07 +0200

    update scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+alias sail='sh $([ -f sail ] && echo sail || echo vendor/bin/sail)'
+
+sail artisan down
+
+sail artisan horizon:terminate --wait
+
+git checkout main
+git pull
+
+# update composer packages
+sail composer i --no-interaction --no-dev --optimize-autoloader
+sail artisan migrate --force
+sail artisan optimize:clear
+sail artisan firebase:download --purge
+sail artisan queue:restart
+
+# build frontend
+sail bun i --production
+sail bun run build
+
+# trigger tasks
+sail artisan trigger-tasks
+
+sail artisan up
