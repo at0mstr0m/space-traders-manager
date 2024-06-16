cce9748afe3bc074ac384b3ccd056bdbcaeeb25d
at0mstr0m <jan.u.hellwig@gmail.com>
2024-06-16 11:20:54 +0200

    rework reset-db script

--- reset-db.sh
+++ reset-db.sh
@@ -1,1 +1,10 @@
-sail artisan optimize:clear && sail artisan cache:redis-flush-all && sail artisan db:wipe && sail artisan migrate && sail artisan db:seed
+#!/bin/bash
+
+alias sail='sh $([ -f sail ] && echo sail || echo vendor/bin/sail)'
+
+sail artisan optimize:clear
+sail artisan cache:redis-flush-all
+sail artisan db:wipe
+sail artisan migrate
+sail artisan db:seed
+sail artisan firebase:download
