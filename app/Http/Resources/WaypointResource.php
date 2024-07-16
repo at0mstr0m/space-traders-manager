5f5905dfa5b9b10a5300e870486c9b6bb5d7045e
at0mstr0m <jan.u.hellwig@gmail.com>
2024-07-16 22:14:49 +0200

    load missing connected jump gate waypoints

--- app/Http/Resources/WaypointResource.php
+++ app/Http/Resources/WaypointResource.php
@@ -2,15 +2,22 @@
 
 namespace App\Http\Resources;
 
+use App\Actions\UpdateWaypointAction;
 use App\Enums\WaypointTypes;
+use App\Helpers\SpaceTraders;
 use App\Models\System;
 use App\Models\Waypoint;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Collection;
 
 class WaypointResource extends JsonResource
 {
     public bool $includeConnectedWaypoints = true;
+
+    private ?Collection $connectedJumpGates = null;
+
+    private SpaceTraders $api;
 
     /**
      * Transform the resource into an array.
@@ -19,6 +26,9 @@ class WaypointResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        /** @var SpaceTraders */
+        $this->api = app(SpaceTraders::class);
+
         return [
             'id' => $this->id,
             'created_at' => $this->created_at->toDateTimeString(),
@@ -32,13 +42,27 @@ class WaypointResource extends JsonResource
             'is_under_construction' => $this->is_under_construction,
             'traits' => WaypointTraitResource::collection($this->traits),
             'ship_count' => $this->whenCounted('ships'),
-            'connected_waypoints' => $this->when(
-                $this->includeConnectedWaypoints
-                && $this->type === WaypointTypes::JUMP_GATE,
-                $this->system->connections->map(
-                    fn (System $system) => static::withoutConnectedWaypoints($system->jumpGate)
-                )
-            ),
+            'connected_waypoints' => $this->includeConnectedWaypoints && $this->type === WaypointTypes::JUMP_GATE
+                ? $this->system->connections->map(function (System $system) {
+                    if ($system->jumpGate) {
+                        return static::withoutConnectedWaypoints($system->jumpGate);
+                    }
+                    $connectedJumpGateSymbols ??= $this->api
+                        ->getJumpGate($this->symbol)
+                        ->connections
+                        ->each(function (string $symbol) {
+                            if (Waypoint::where('symbol', $symbol)->doesntExist()) {
+                                $jumpGate = UpdateWaypointAction::run(
+                                    $this->api->getWaypoint($symbol)
+                                );
+                            }
+                        });
+
+                    // $jumpGate = UpdateWaypointAction::run(
+                    //     $this->api->getWaypoint($this->symbol)
+                    // );
+                })
+                : null,
         ];
     }
 
